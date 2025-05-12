8f506e4ba92c554251b848236a4881738747bb1b
LinkedBear <742532002@qq.com>
2025-05-13 07:01:35 +0800

    Initial commit

---
+++ spring-bean-scope/src/main/java/com/linkedbear/spring/scope/SpringBeanScopeApplication.java
@@ -0,0 +1,12 @@
+package com.linkedbear.spring.scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanScopeApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanScopeApplication.class, args);
+    }
+}
